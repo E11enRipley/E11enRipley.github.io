82c1cdec2218e421ae9eb82b0fde1f046afc87c9
Leigh Dixon <leigh.dixon@premiers.qld.gov.au>
2021-12-16 11:06:20 +1000

    Initial commit

---
+++ js/dataset.js
@@ -0,0 +1,386 @@
+dataset = {
+    "records":
+    [
+
+{
+    "cat": "microsoft",
+    "text": "Azure",
+    "icon": "fab fa-microsoft",
+    "link": "https://portal.azure.com/#home"
+},
+{
+    "cat": "microsoft",
+    "text": "Cloud App Security",
+    "icon": "fab fa-microsoft",
+    "link": "https://dpcqld.portal.cloudappsecurity.com/#/dashboard"
+},
+{
+    "cat": "microsoft",
+    "text": "Security & Compliance",
+    "icon": "fab fa-microsoft",
+    "link": "https://protection.office.com/homepage"
+},
+{
+    "cat": "microsoft",
+    "text": "Defender ATP",
+    "icon": "fab fa-microsoft",
+    "link": "https://securitycenter.windows.com/dashboard"
+},
+{
+    "cat": "microsoft",
+    "text": "Quarantine",
+    "icon": "fas fa-biohazard",
+    "link": "https://protection.office.com/quarantine"
+},
+{
+    "cat": "microsoft",
+    "text": "Sentinel",
+    "icon": "svgSentinel",
+    "link": "https://portal.azure.com/#blade/Microsoft_Azure_Security_Insights/MainMenuBlade/0/subscriptionId/e472542b-3ba3-461b-9754-99ea000ff7df/resourceGroup/DPC_Sentinel_Prod_RG/workspaceName/DPCAzureSentinel"
+},
+
+{
+    "cat": "products",
+    "text": "Carbon Black",
+    "icon": "fas fa-shield-alt",
+    "link": "https://cbawl.premiers.qld.gov.au/Dashboard/Dashboard/Dashboard.aspx"
+},
+{
+    "cat": "products",
+    "text": "zScaler",
+    "icon": "fas fa-shield-alt",
+    "link": "https://admin.zscalertwo.net/#dashboard/1"
+},
+{
+    "cat": "products",
+    "text": "ProofPoint",
+    "icon": "fas fa-fish",
+    "link": "https://premiersqld.securityeducation.com/"
+},
+{
+    "cat": "security",
+    "text": "FireEye",
+    "icon": "fas fa-newspaper",
+    "link": "https://www.fireeye.com/blog.html"
+},
+{
+    "cat": "security",
+    "text": "MailGuard",
+    "icon": "fas fa-newspaper",
+    "link": "https://www.mailguard.com.au/blog"
+},
+{
+    "cat": "security",
+    "text": "Threatpost",
+    "icon": "fas fa-newspaper",
+    "link": "https://threatpost.com/"
+},
+{
+    "cat": "security",
+    "text": "Hacker News",
+    "icon": "fas fa-newspaper",
+    "link": "https://thehackernews.com/"
+},
+{
+    "cat": "security",
+    "text": "ACSC",
+    "icon": "fas fa-user-secret",
+    "link": "https://www.cyber.gov.au/"
+},
+{
+    "cat": "security",
+    "text": "ACSC Essential 8",
+    "icon": "fas fa-user-secret",
+    "link": "https://www.cyber.gov.au/publications/essential-eight-maturity-model"
+},
+{
+    "cat": "security",
+    "text": "CYWARE",
+    "icon": "fas fa-newspaper",
+    "link": "https://cyware.com/cyber-security-news-articles"
+},
+{
+    "cat": "security",
+    "text": "Naked Security",
+    "icon": "fas fa-newspaper",
+    "link": "https://nakedsecurity.sophos.com/"
+},
+{
+    "cat": "security",
+    "text": "Use a passphrase",
+    "icon": "fas fa-key",
+    "link": "https://www.useapassphrase.com/"
+},
+{
+    "cat": "security",
+    "text": "any.run",
+    "icon": "fas fa-key",
+    "link": "https://app.any.run/"
+},
+
+
+{
+    "cat": "news",
+    "text": "Feedly",
+    "icon": "fas fa-rss",
+    "link": "https://feedly.com/i/my"
+},
+{
+    "cat": "hunting",
+    "text": "Virus Total",
+    "icon": "fas fa-spider",
+    "link": "https://www.virustotal.com/gui/home/upload"
+},
+{
+    "cat": "hunting",
+    "text": "urlscan.io",
+    "icon": "fas fa-search-location",
+    "link": "https://urlscan.io/"
+},
+{
+    "cat": "hunting",
+    "text": "hybrid-analysis.com",
+    "icon": "fas fa-search-location",
+    "link": "https://www.hybrid-analysis.com/"
+},
+{
+    "cat": "hunting",
+    "text": "url encode/decode",
+    "icon": "fab fa-elementor",
+    "link": "https://www.url-encode-decode.com/"
+},
+{
+    "cat": "hunting",
+    "text": "Message Analyzer",
+    "icon": "fab fa-microsoft",
+    "link": "https://mha.azurewebsites.net/"
+},
+{
+    "cat": "hunting",
+    "text": "IP Info",
+    "icon": "fas fa-search-location",
+    "link": "https://ipinfo.io/"
+},
+{
+    "cat": "hunting",
+    "text": "ThreatCloud",
+    "icon": "fab fa-searchengin",
+    "link": "https://www.threatcrowd.org/"
+},
+{
+    "cat": "hunting",
+    "text": "Check short URL",
+    "icon": "fas fa-search",
+    "link": "http://www.checkshorturl.com/"
+},
+{
+    "cat": "hunting",
+    "text": "Uncoder.IO",
+    "icon": "fas fa-search",
+    "link": "https://uncoder.io/"
+},
+
+
+
+
+
+{
+    "cat": "work",
+    "text": "Aurion",
+    "icon": "fas fa-user-clock",
+    "link": "https://ess.aurion.qgs.qld.gov.au/aurprd/actionitems"
+},
+{
+    "cat": "work",
+    "text": "Datacom",
+    "icon": "fas fa-list",
+    "link": "https://myservice.datacom.com.au/CherwellClient/Access?_=6ba938ff#0"
+},
+{
+    "cat": "work",
+    "text": "JLM",
+    "icon": "fas fa-address-book",
+    "link": "http://jlm"
+},
+{
+    "cat": "medium",
+    "text": "corona map",
+    "icon": "fas fa-hospital ",
+    "link": "https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6"
+},
+{
+    "cat": "medium",
+    "text": "abc.net.au",
+    "icon": "fas fa-newspaper ",
+    "link": "https://www.abc.net.au"
+},
+{
+    "cat": "news",
+    "text": "news.com.au",
+    "icon": "fas fa-newspaper ",
+    "link": "https://www.news.com.au"
+},
+{
+    "cat": "news",
+    "text": "brisbanetimes.com.au",
+    "icon": "fas fa-newspaper ",
+    "link": "https://www.brisbanetimes.com.au/"
+},
+{
+    "cat": "news",
+    "text": "itnews.com.au",
+    "icon": "fas fa-newspaper ",
+    "link": "https://www.itnews.com.au/"
+},
+{
+    "cat": "news",
+    "text": "abc.net.au",
+    "icon": "fas fa-newspaper ",
+    "link": "https://www.abc.net.au/news"
+},
+{
+    "cat": "socials",
+    "text": "Twitter",
+    "icon": "fab fa-twitter  fa-2x'></i>",
+    "link": "https://twitter.com/"
+},
+{
+    "cat": "socials",
+    "text": "Reddit",
+    "icon": "fab fa-reddit-alien  fa-2x'></i>",
+    "link": "https://old.reddit.com/new"
+},
+{
+    "cat": "socials",
+    "text": "FaceBook",
+    "icon": "fab fa-facebook-f  fa-2x'></i>",
+    "link": "https://www.facebook.com"
+},
+
+{
+    "cat": "work",
+    "text": "Building Invitiations",
+    "icon": "far fa-building  fa-2x'></i>",
+    "link": "https://dashboard.sine.co/#/"
+},
+{
+    "cat": "work",
+    "text": "Book meeting rooms",
+    "icon": "fas fa-handshake  fa-2x'></i>",
+    "link": "https://bookings.qgaaad.qld.gov.au/Master.aspx"
+},
+{
+    "cat": "google",
+    "text": "Messages",
+    "icon": "svgGmail",
+    "link": "https://messages.google.com/"
+},
+{
+    "cat": "google",
+    "text": "Gmail",
+    "icon": "svgGmail",
+    "link": "https://mail.google.com/mail/u/0/"
+},
+{
+    "cat": "google",
+    "text": "Calendar",
+    "icon": "svgCal",
+    "link": "https://calendar.google.com/calendar/r?pli=1"
+},
+{
+    "cat": "google",
+    "text": "Hangouts",
+    "icon": "svgHangouts",
+    "link": "https://hangouts.google.com/"
+},
+{
+    "cat": "google",
+    "text": "Photos",
+    "icon": "svgPhotos",
+    "link": "https://photos.google.com/"
+},
+{
+    "cat": "google",
+    "text": "Podcasts",
+    "icon": "svgPhotos",
+    "link": "https://podcasts.google.com/"
+},
+{
+    "cat": "tools",
+    "text": "Kindle",
+    "icon": "svgKindle",
+    "link": "https://read.amazon.com/"
+},
+{
+    "cat": "google",
+    "text": "Drive",
+    "icon": "svgDrive",
+    "link": "https://drive.google.com/"
+},
+{
+    "cat": "google",
+    "text": "Keep",
+    "icon": "svgKeep",
+    "link": "https://keep.google.com/"
+},
+{
+    "cat": "security",
+    "text": "MITRE ATT&CK",
+    "icon": "",
+    "link": "https://attack.mitre.org/matrices/enterprise/"
+},
+
+{
+    "cat": "microsoft",
+    "text": "Admin Centers",
+    "icon": "",
+    "link": "https://portal.office.com/Adminportal/Home#/alladmincenters"
+}
+
+,
+
+{
+    "cat": "learning",
+    "text": "Udemy",
+    "icon": "",
+    "link": "https://www.udemy.com/"
+},
+{
+    "cat": "learning",
+    "text": "awesome-security",
+    "icon": "",
+    "link": "https://github.com/sbilly/awesome-security"
+},
+{
+    "cat": "learning",
+    "text": "isc2 (cissp)",
+    "icon": "",
+    "link": "https://learn.isc2.org/d2l/home"
+},
+
+{
+    "cat": "learning",
+    "text": "pluralsight",
+    "icon": "",
+    "link": "https://app.pluralsight.com"
+},
+
+
+
+{
+    "cat": "tools",
+    "text": "Down Detector",
+    "icon": "fab fa-dropbox  fa-2x'></i>",
+    "link": "https://downdetector.com.au/"
+}
+
+
+
+
+
+
+
+
+
+
+]};
