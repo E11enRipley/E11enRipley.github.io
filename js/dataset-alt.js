57a40617f461fd52e00dbfdbca0e1acfa57e329b
E11enRipley <96213381+E11enRipley@users.noreply.github.com>
2021-12-21 11:25:50 +1000

    New landing data

---
+++ js/dataset-alt.js
@@ -0,0 +1,424 @@
+dataset = {
+    "records":
+    [
+        ////////////////////////////////////////
+        //microsoft
+        {   "cat": "microsoft", 
+            "text":"Azure", 
+            "icon":"fab fa-microsoft", 
+            "link":"https://portal.azure.com"
+        },
+        {   "cat": "microsoft", 
+            "text":"Sentinel", 
+            "icon":"fab fa-microsoft", 
+            "link":"https://portal.azure.com/#blade/Microsoft_Azure_Security_Insights/MainMenuBlade/0/subscriptionId/e472542b-3ba3-461b-9754-99ea000ff7df/resourceGroup/DPC_Sentinel_Prod_RG/workspaceName/DPCAzureSentinel"
+        },
+        {   "cat": "microsoft", 
+            "text":"Cloud App Security", 
+            "icon":"fab fa-microsoft", 
+            "link":"https://dpcqld.portal.cloudappsecurity.com"
+        },
+        {   "cat": "microsoft", 
+            "text":"Security & Compliance", 
+            "icon":"fab fa-microsoft", 
+            "link":"https://dpcqld.portal.cloudappsecurity.com"
+        },
+        {   "cat": "microsoft", 
+            "text":"Defender ATP", 
+            "icon":"fab fa-microsoft", 
+            "link":"https://securitycenter.windows.com/dashboard"
+        },
+
+
+
+
+        ////////////////////////////////////////
+        //news
+        {
+            "cat": "news",
+            "text": "Threatpost",
+            "icon": "fas fa-newspaper",
+            "link": "https://threatpost.com/"
+        },
+        {
+            "cat": "news",
+            "text": "Hacker News",
+            "icon": "fab fa-hacker-news-square",
+            "link": "https://thehackernews.com/"
+        },
+        {
+            "cat": "news",
+            "text": "Feedly",
+            "icon": "fas fa-rss",
+            "link": "https://feedly.com/i/my"
+        },
+        {
+            "cat": "news",
+            "text": "news.com.au",
+            "icon": "fas fa-newspaper ",
+            "link": "https://www.news.com.au"
+        },
+        {
+            "cat": "news",
+            "text": "brisbanetimes.com.au",
+            "icon": "fas fa-newspaper ",
+            "link": "https://www.brisbanetimes.com.au/"
+        },
+        {
+            "cat": "news",
+            "text": "itnews.com.au",
+            "icon": "fas fa-newspaper ",
+            "link": "https://www.itnews.com.au/"
+        },
+        {
+            "cat": "news",
+            "text": "abc.net.au",
+            "icon": "fas fa-newspaper ",
+            "link": "https://www.abc.net.au/news"
+        },
+
+
+
+        ////////////////////////////////////////
+        //hunting
+
+        {
+            "cat": "hunting",
+            "text": "urlscan.io",
+            "icon": "fas fa-search-location",
+            "link": "https://urlscan.io/"
+        },
+        {
+            "cat": "hunting",
+            "text": "Cyber Chef",
+            "icon": "fab fa-elementor",
+            "link": "https://gchq.github.io/CyberChef/"
+        },
+        {
+            "cat": "hunting",
+            "text": "Message Analyzer",
+            "icon": "fab fa-microsoft",
+            "link": "https://mha.azurewebsites.net/"
+        },
+        {
+            "cat": "hunting",
+            "text": "ThreatCloud",
+            "icon": "fab fa-searchengin",
+            "link": "https://www.threatcrowd.org/"
+        },
+        {
+            "cat": "hunting",
+            "text": "Check short URL",
+            "icon": "fas fa-search",
+            "link": "http://www.checkshorturl.com/"
+        },
+        {
+            "cat": "hunting",
+            "text": "Uncoder.IO",
+            "icon": "fas fa-search",
+            "link": "https://uncoder.io/"
+        },
+        {
+            "cat": "hunting",
+            "text": "AlienVault",
+            "icon": "fas fa-search",
+            "link": "https://otx.alienvault.com/browse/global/indicators"
+        },
+        {
+            "cat": "hunting",
+            "text": "Shodan",
+            "icon": "fas fa-search",
+            "link": "https://www.shodan.io/"
+        },
+
+
+        ////////////////////////////////////////
+        //malware
+        {
+            "cat": "malware analysis",
+            "text": "any.run",
+            "icon": "fas fa-spider",
+            "link": "https://app.any.run/"
+        },
+        {
+            "cat": "malware analysis",
+            "text": "Triage",
+            "icon": "fas fa-spider",
+            "link": "https://tria.ge/"
+        },
+        {
+            "cat": "malware analysis",
+            "text": "Joe Sandbox",
+            "icon": "fas fa-spider",
+            "link": "https://www.joesandbox.com/#windows"
+        },
+        {
+            "cat": "malware analysis",
+            "text": "Hybrid Analysis",
+            "icon": "fas fa-spider",
+            "link": "https://www.hybrid-analysis.com/"
+        },
+        {
+            "cat": "malware analysis",
+            "text": "Virus Total",
+            "icon": "fas fa-spider",
+            "link": "https://www.virustotal.com/gui/home/upload"
+        },
+        {
+            "cat": "malware analysis",
+            "text": "Cuckoo",
+            "icon": "fas fa-spider",
+            "link": "https://sandbox.pikker.ee/"
+        },
+        {
+            "cat": "malware analysis",
+            "text": "Abuse.ch",
+            "icon": "fas fa-spider",
+            "link": "https://abuse.ch/"
+        },
+
+                ////////////////////////////////////////
+        //security
+        {
+            "cat": "security",
+            "text": "FireEye",
+            "icon": "fas fa-newspaper",
+            "link": "https://www.fireeye.com/blog.html"
+        },
+        {
+            "cat": "security",
+            "text": "MailGuard",
+            "icon": "fas fa-newspaper",
+            "link": "https://www.mailguard.com.au/blog"
+        },
+        {
+            "cat": "security",
+            "text": "Threatpost",
+            "icon": "fas fa-newspaper",
+            "link": "https://threatpost.com/"
+        },
+        {
+            "cat": "security",
+            "text": "Hacker News",
+            "icon": "fas fa-newspaper",
+            "link": "https://thehackernews.com/"
+        },
+        {
+            "cat": "security",
+            "text": "ACSC",
+            "icon": "fas fa-user-secret",
+            "link": "https://www.cyber.gov.au/"
+        },
+        {
+            "cat": "security",
+            "text": "ACSC Essential 8",
+            "icon": "fas fa-user-secret",
+            "link": "https://www.cyber.gov.au/publications/essential-eight-maturity-model"
+        },
+        {
+            "cat": "security",
+            "text": "CYWARE",
+            "icon": "fas fa-newspaper",
+            "link": "https://cyware.com/cyber-security-news-articles"
+        },
+        {
+            "cat": "security",
+            "text": "Naked Security",
+            "icon": "fas fa-newspaper",
+            "link": "https://nakedsecurity.sophos.com/"
+        },
+        {
+            "cat": "security",
+            "text": "Use a passphrase",
+            "icon": "fas fa-key",
+            "link": "https://www.useapassphrase.com/"
+        },
+
+
+                ////////////////////////////////////////
+        //IP/DNS
+        {
+            "cat": "ip/dns",
+            "text": "IP Info",
+            "icon": "fas fa-search-location",
+            "link": "https://ipinfo.io/"
+        },
+        {
+            "cat": "ip/dns",
+            "text": "IP Toolbox",
+            "icon": "fas fa-search-location",
+            "link": "https://mxtoolbox.com/SuperTool.aspx"
+        },
+        {
+            "cat": "ip/dns",
+            "text": "Spamhaus",
+            "icon": "fas fa-search-location",
+            "link": "https://check.spamhaus.org/"
+        },
+        {
+            "cat": "ip/dns",
+            "text": "Symantec IP Reputation",
+            "icon": "fas fa-search-location",
+            "link": "https://ipremoval.sms.symantec.com/lookup"
+        },
+        {
+            "cat": "ip/dns",
+            "text": "Talos Intelligence",
+            "icon": "fas fa-search-location",
+            "link": "https://talosintelligence.com/"
+        },
+        {
+            "cat": "ip/dns",
+            "text": "DNS Twister",
+            "icon": "fas fa-search-location",
+            "link": "https://dnstwister.report/"
+        },
+
+                ////////////////////////////////////////
+        //work
+        {
+            "cat": "work/products",
+            "text": "Carbon Black",
+            "icon": "fas fa-shield-alt",
+            "link": "https://cbawl.premiers.qld.gov.au/Dashboard/Dashboard/Dashboard.aspx"
+        },
+        {
+            "cat": "work/tools",
+            "text": "zScaler",
+            "icon": "fas fa-shield-alt",
+            "link": "https://admin.zscalertwo.net/#dashboard/1"
+        },
+        {
+            "cat": "work/tools",
+            "text": "ProofPoint",
+            "icon": "fas fa-fish",
+            "link": "https://premiersqld.securityeducation.com/"
+        },
+        {
+            "cat": "work/tools",
+            "text": "Awesome TechStack",
+            "icon": "fas fa-building",
+            "link": "https://awesometechstack.com/"
+        },        
+        {
+            "cat": "work/tools",
+            "text": "Built With",
+            "icon": "fas fa-building",
+            "link": "https://builtwith.com/"
+        },        
+        {
+            "cat": "work/tools",
+            "text": "Site Report",
+            "icon": "fas fa-building",
+            "link": "https://sitereport.netcraft.com/"
+        }, 
+
+        ////////////////////////////////////////
+        //social
+        {
+            "cat": "social",
+            "text": "Twitter",
+            "icon": "fab fa-twitter  fa-2x'></i>",
+            "link": "https://twitter.com/"
+        },
+        {
+            "cat": "social",
+            "text": "Reddit",
+            "icon": "fab fa-reddit-alien  fa-2x'></i>",
+            "link": "https://old.reddit.com/new"
+        },
+        {
+            "cat": "social",
+            "text": "FaceBook",
+            "icon": "fab fa-facebook-f  fa-2x'></i>",
+            "link": "https://www.facebook.com"
+        },
+        {
+            "cat": "social",
+            "text": "LinkedIn",
+            "icon": "fab fa-linkedin  fa-2x'></i>",
+            "link": "https://www.linkedin.com.au"
+        },
+
+
+        ////////////////////////////////////////
+        //google
+        {
+            "cat": "google",
+            "text": "Messages",
+            "icon": "fas fa-sms",
+            "link": "https://messages.google.com/"
+        },
+        {
+            "cat": "google",
+            "text": "Gmail",
+            "icon": "fas fa-envelope",
+            "link": "https://mail.google.com/mail/u/0/"
+        },
+        {
+            "cat": "google",
+            "text": "Calendar",
+            "icon": "far fa-calendar",
+            "link": "https://calendar.google.com/calendar/r?pli=1"
+        },
+        {
+            "cat": "google",
+            "text": "Hangouts",
+            "icon": "far fa-comments",
+            "link": "https://hangouts.google.com/"
+        },
+        {
+            "cat": "google",
+            "text": "Photos",
+            "icon": "fas fa-camera-retro",
+            "link": "https://photos.google.com/"
+        },
+        {
+            "cat": "google",
+            "text": "Podcasts",
+            "icon": "fas fa-podcast",
+            "link": "https://podcasts.google.com/"
+        },
+        {
+            "cat": "google",
+            "text": "Drive",
+            "icon": "fab fa-google-drive",
+            "link": "https://drive.google.com/"
+        },
+        {
+            "cat": "google",
+            "text": "Keep",
+            "icon": "fas fa-tasks",
+            "link": "https://keep.google.com/"
+        },
+
+        ////////////////////////////////////////
+        //learning
+        {
+            "cat": "learning",
+            "text": "Udemy",
+            "icon": "fas fa-chalkboard-teacher",
+            "link": "https://www.udemy.com/"
+        },
+        {
+            "cat": "learning",
+            "text": "awesome-security",
+            "icon": "fab fa-github",
+            "link": "https://github.com/sbilly/awesome-security"
+        },
+        {
+            "cat": "learning",
+            "text": "isc2 (cissp)",
+            "icon": "fas fa-chalkboard-teacher",
+            "link": "https://learn.isc2.org/d2l/home"
+        },
+        {
+            "cat": "learning",
+            "text": "pluralsight",
+            "icon": "fas fa-chalkboard-teacher",
+            "link": "https://app.pluralsight.com"
+        }
+
+
+
+]};
