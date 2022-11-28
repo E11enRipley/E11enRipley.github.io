e6496ce7a73c0d4a1a7f21779db8fcd2cbf9ed6c
E11en Ripley <leighmdixon@gmail.com>
2022-11-28 12:02:04 +1000

    another dirty

--- js/dataset-alt.js
+++ js/dataset-alt.js
@@ -664,7 +664,12 @@ dataset = {
             //"icon": "fas fa-chalkboard-teacher",
             "link": "https://app.pluralsight.com"
         }
-
-
+        {
+            "cat": "learning",
+            "text": "Blue Team Labs",
+            //"icon": "fas fa-chalkboard-teacher",
+            "link": "https://blueteamlabs.online/"
+        }
+        
 
 ]};
